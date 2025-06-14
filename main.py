5fbbc461152243822b5791c1d8c8da158f58bfd5
greetingromansoldier <greetingromansoldier@proton.me>
2025-06-14 20:19:18 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', 
+    contents='Why is the sky blue? Only one paragraph'
+)
+print(response.text)
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
